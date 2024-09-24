eb2d2d6d2711fe9c85c5643f9835b1066c734d19
Osama Soliman <156087854+OsamaSoliman722@users.noreply.github.com>
2024-09-24 23:31:11 +0300

    LoginPage

---
+++ src/compnanat/LoginPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './LoginPage.css';
+export default function LoginPage() {
+    return (
+        <div className='parent'>
+            <form>
+                <div>
+                    <div >
+                        <h3>Login</h3>
+                        <label htmlFor=""> </label>
+                        <input type="email" placeholder="Enter Your Email" />
+
+                    </div>
+                    <div >
+                        <label htmlFor=""> </label>
+
+                        <input type="password" placeholder="Enter Your Password" />
+                    </div>
+                </div>
+                
+                   
+                    <a href="#" className='href'> Forget Password?</a>
+               
+                <div>
+                    <button type='submit'>Login</button>
+                </div>
+                <div className='register-link'>
+                    <span>Don't Have An Account? <a href="#" className='register'>Register</a></span>
+                    
+                </div>
+            </form>
+        </div >
+    )
+}
